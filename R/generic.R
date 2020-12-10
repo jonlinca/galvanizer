badfafe992247704609163b553fa47416d66b779
Jon Lin <jonlinca@users.noreply.github.com>
2020-12-10 08:33:10 -0700

    Add in a pause if there is a specific gateway timeout

--- R/generic.R
+++ R/generic.R
@@ -63,11 +63,15 @@ hb_api_get <- function(auth, url, params = NULL){
                             query = params)
     }
     
-    # Force a retry if it fails
+    # Force a retry if it fails due to time out. These two catches will bypass the validate, preventing a complete stop
     if (httr::status_code(hb_api_get) == 429){
       message("Rate limit exceeded, waiting to retry")
       Sys.sleep(0.1)
-    } else {
+    } else if (httr::status_code(hb_api_get) == 504){
+      message("Gateway timeout, waiting to retry")
+      Sys.sleep(0.2)
+    } 
+    else {
       check_download <- hb_validateDownload(hb_api_get)
       if (check_download){skip_retry <- FALSE}
     }
